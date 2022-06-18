ac7c1ac3394c292ce56cbc5220b86739c83e009a
HBchevelle68 <aportante10@gmail.com>
2022-06-18 17:15:11 -0400

    storing

--- go/ls/readdir_walk/ls_readdir.go
+++ go/ls/readdir_walk/ls_readdir.go
@@ -48,18 +48,86 @@ func lsTimeStr(time time.Time) string {
 	return fmt.Sprintf("%s", time.Format("MMM DD YYYY HH:SS"))
 }
 
-func buildPermStr(file_mode uint32) {
-    permStr = ""
-    permStr += (file_mode & syscall.S_IRUSR ) ? 'r' : '-';
-    permStr = (file_mode & S_IWUSR) ? 'w' : '-';
-    permStr = (file_mode & S_IXUSR) ? 'x' : '-';
-    permStr = (file_mode & S_IRGRP) ? 'r' : '-';
-    permStr = (file_mode & S_IWGRP) ? 'w' : '-';
-    permStr = (file_mode & S_IXGRP) ? 'x' : '-';
-    permStr = (file_mode & S_IROTH) ? 'r' : '-';
-    permStr = (file_mode & S_IWOTH) ? 'w' : '-';
-    permStr = (file_mode & S_IXOTH) ? 'x' : '-';
-    permStr = '\0';
+var g_permMap = map[int]string{
+	syscall.S_IRUSR: "r",
+	syscall.S_IWUSR: "w",
+	syscall.S_IXUSR: "x",
+	syscall.S_IRGRP: "r",
+	syscall.S_IWGRP: "w",
+	syscall.S_IXGRP: "x",
+	syscall.S_IROTH: "r",
+	syscall.S_IWOTH: "w",
+	syscall.S_IXOTH: "x",
+}
+
+/* Keys to efficiently iterate the g_permMap
+ * Since these are known are build time
+ * and order matter and will not change
+ * Placing these here
+ */
+var g_keys = [9]uint32{
+	uint32(syscall.S_IRUSR),
+	uint32(syscall.S_IWUSR),
+	uint32(syscall.S_IXUSR),
+	uint32(syscall.S_IRGRP),
+	uint32(syscall.S_IWGRP),
+	uint32(syscall.S_IXGRP),
+	uint32(syscall.S_IROTH),
+	uint32(syscall.S_IWOTH),
+	uint32(syscall.S_IXOTH),
+}
+
+func buildTypePermStr(file_mode uint32) string {
+	// Type and permissions string (tps)
+	tps := ""
+	var mask uint32 = 0x0100
+
+	switch file_mode & syscall.S_IFMT {
+	case syscall.S_IFIFO:
+		tps += "p"
+	case syscall.S_IFCHR:
+		tps += "c"
+	case syscall.S_IFDIR:
+		tps += "d"
+	case syscall.S_IFBLK:
+		tps += "b"
+	case syscall.S_IFREG:
+		tps += "-"
+	case syscall.S_IFLNK:
+		tps += "l"
+	case syscall.S_IFSOCK:
+		tps += "s"
+	default:
+		fmt.Printf("ERROR INVALID MASK FOUND: %d\n", file_mode&syscall.S_IFMT)
+		return tps
+	}
+
+	for i := 0; i < 9; i++ {
+		switch file_mode & (mask >> i) {
+		case syscall.S_IRUSR:
+			tps += "r"
+		case syscall.S_IWUSR:
+			tps += "w"
+		case syscall.S_IXUSR:
+			tps += "x"
+		case syscall.S_IRGRP:
+			tps += "r"
+		case syscall.S_IWGRP:
+			tps += "w"
+		case syscall.S_IXGRP:
+			tps += "x"
+		case syscall.S_IROTH:
+			tps += "r"
+		case syscall.S_IWOTH:
+			tps += "w"
+		case syscall.S_IXOTH:
+			tps += "x"
+		default:
+			tps += "-"
+		}
+	}
+
+	return tps
 }
 
 func main() {
@@ -84,9 +152,11 @@ func main() {
 
 	for _, file := range dirlist {
 
+		var fstat = syscall.Stat_t{}
+
 		fpath := filepath.Join(abspath, file.Name())
 
-		if fi, err := os.Stat(fpath); err != nil {
+		if err := syscall.Stat(fpath, &fstat); err != nil {
 
 			if errno, ok := err.(syscall.Errno); ok {
 
@@ -98,18 +168,19 @@ func main() {
 				continue
 			}
 		} else {
-			fstat := fi.Sys().(*syscall.Stat_t)
 			grp, _ := gidToGrpStuct(fstat.Gid)
 			usr, _ := uidToUserStuct(fstat.Uid)
-			mode := buildPermStr(fstat.Mode)
+			mode := buildTypePermStr(uint32(fstat.Mode))
+
+			//fstat.Mtim.
 			fmt.Printf(
 				"%s %3d %4s %4s %10d %15s %-s\n",
-				fi.Mode(),
+				mode,
 				fstat.Nlink,
 				grp.Name,
 				usr.Name,
 				fstat.Size,
-				fi.ModTime().Format("Jan 01"),
+				"0",
 				fpath,
 			)
 		}
