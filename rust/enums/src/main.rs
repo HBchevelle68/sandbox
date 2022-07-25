d06fd6f43441079b83da1da59c056963042dea48
HBchevelle68 <aportante10@gmail.com>
2022-07-25 19:38:53 -0400

    storing

---
+++ rust/enums/src/main.rs
@@ -0,0 +1,50 @@
+#![allow(unused)]
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    println!("Hello, Enums!");
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+    println!("{:?}", some_number);
+
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State Quarter from {:?}!", state);
+            25
+        }
+    }
+}
