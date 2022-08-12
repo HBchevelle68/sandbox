cbcf0560f3b961a65cbbe51f49f1e2d6393e5e12
HBchevelle68 <aportante10@gmail.com>
2022-08-11 21:11:56 -0400

    storing

--- rust/strings/src/main.rs
+++ rust/strings/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("Hello ");
+    let s2 = String::from("world");
+    let s3 = String::from("!");
+
+    let s4 = s1 + &s2 + &s3;
+
+    dbg!(&s4);
+
+    println!("String: {:?}", &s4);
 }
